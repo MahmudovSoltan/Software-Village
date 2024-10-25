dfff6d736fca4c7f97ec360391963197e065d10b
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-10-26 02:12:11 +0400

    training, about, why us , blog section

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-const mainDiv = document.querySelector("#root")
+// ROUTTER
+
+const mainDiv = document.querySelector("#root");
 const routes = {
   "": {
     title: "home",
-    data: "./Pages/home.html"
+    data: "./Pages/home.html",
   },
   "#trainingprogram": {
     title: "trainingprogram",
-    data: "./Pages/trainingprogram.html"
+    data: "./Pages/trainingprogram.html",
   },
   "#about": {
     title: "about",
-    data: "./Pages/about.html"
+    data: "./Pages/about.html",
   },
   "#quetions": {
     title: "quetions",
-    data: "./Pages/quetions.html"
+    data: "./Pages/quetions.html",
   },
   "#contact": {
     title: "contact",
-    data: "./Pages/contact.html"
-  }
-}
+    data: "./Pages/contact.html",
+  },
+};
 
 function chechRout(hash = window.location.hash) {
   if (!hash) {
@@ -29,17 +31,32 @@ function chechRout(hash = window.location.hash) {
   document.title = routes[hash].title;
   const dataUrl = routes[hash].data;
   fetch(dataUrl)
-    .then(res => res.text())
-    .then(data => mainDiv.innerHTML = data);
+    .then((res) => res.text())
+    .then((data) => (mainDiv.innerHTML = data));
 }
 
 window.addEventListener("load", () => {
   chechRout();
 
   const links = document.querySelectorAll("a"); // Linkləri load hadisəsi içində tapırıq
-  links.forEach(link => link.addEventListener("click", (event) => {
-    event.preventDefault(); // Default davranışı bloklamaq üçün
-    const hash = link.getAttribute("href"); // href-i əldə edirik
-    chechRout(hash);
-  }));
+  links.forEach((link) =>
+    link.addEventListener("click", (event) => {
+      event.preventDefault(); // Default davranışı bloklamaq üçün
+      const hash = link.getAttribute("href"); // href-i əldə edirik
+      chechRout(hash);
+    })
+  );
+  const home_slider = document.querySelector(".home_slider");
+  const homecarusel = document.querySelector(".home_carusel");
+  const carusel_btn = document.querySelectorAll(".carusel_btn");
+  console.log(homecarusel);
+
+  carusel_btn.forEach((item) =>
+    item.addEventListener("click", () => {
+      // home_slider.style.tarnsform = translatex("100px")
+      console.log("btn click");
+    })
+  );
 });
+
+// SLIDER JS
